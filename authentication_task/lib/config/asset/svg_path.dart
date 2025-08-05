03ead216b2d791ffef28e12288d1b61a0f68437a
hdsneshati <hadisneshati1384@gmail.com>
2025-08-05 21:48:25 +0330

    asset font & icon

--- authentication_task/lib/config/asset/svg_path.dart
+++ authentication_task/lib/config/asset/svg_path.dart
@@ -1,5 +1,14 @@
 class SvgPath {
   SvgPath._();
-  
+
+
+  static const String apple = "assets/svgs/Applesvg.svg";
+  static const String google = "assets/svgs/google.svg";
+  static const String facebook = "assets/svgs/Facebook.svg";
+  static const String splash = "assets/svgs/splashsvg.svg";
+   static String connection = "assets/svgs/connection.svg";
+    static const String logo = "assets/svgs/logo.svg";
+    static const String ap = "assets/svgs/Apple svg (1).svg";
+
   
 }
--- authentication_task/lib/feature/presentation/intro/screens/splash_screen.dart
+++ authentication_task/lib/feature/presentation/intro/screens/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:authentication_task/config/asset/svg_path.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,52 +14,35 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(seconds: 
-    5), () {
-    //  Navigator.pushReplacement(
-       // context,
-       // MaterialPageRoute(builder: (context) { }),
-     // );
+    Future.delayed(const Duration(seconds: 115), () {
+      // Navigator.pushReplacement(
+      //  context,
+      //  MaterialPageRoute(builder: (context) => const LoginScreen()),
+      // );
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: Column(
-      children: [
-        
-        Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                 
-               //   ),
-                 // DelayedWidget(
-                  //  delayDuration: const Duration(milliseconds: 700),
-                   // animationDuration: Durations.long2,
-                  //  animation: DelayedAnimations.SLIDE_FROM_RIGHT,
-                  //  child: Shimmer.fromColors(
-                  //    period: Durations.extralong4,
-                   //   direction: ShimmerDirection.rtl,
-                   //   baseColor: Theme.of(context).colorScheme.onSurface,
-                   //   highlightColor: Theme.of(context).colorScheme.surfaceTint,
-                     // child:
-                       const Text(
-                        "الـگو",
-                        style: TextStyle(
-                          fontSize: 100,
-                          height: 7,
-                          fontFamily: "dana",
-                          fontWeight: FontWeight.w600,
+      backgroundColor: Theme.of(context).colorScheme.primary,
+                         
+      body: Column(
+        children: [
+          Align(
+              alignment: Alignment.center,
+              child: SvgPicture.asset(
+                SvgPath.ap,
+                width: 100,
+                height: 100,
+                 colorFilter: ColorFilter.mode(
+                         
+                               Theme.of(context).colorScheme.onPrimary,
+                          BlendMode.srcIn,
                         ),
-                      ),
-                  //  ),
-                 // ),
-                ],
-              ),
-      
-      ],
-     ),
-      
-      );
+              )),
+        ],
+      ),
+    );
   }
 }
