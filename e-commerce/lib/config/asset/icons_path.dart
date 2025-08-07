a506411ceb8d02adcbcb1d1a2d1951e65c6ce918
hdsneshati <hadisneshati1384@gmail.com>
2025-08-07 18:15:06 +0330

    add img

--- e-commerce/lib/config/asset/icons_path.dart
+++
@@ -1,9 +0,0 @@
-class IconPath {
-  IconPath._();
-
-  static const String ap = "assets/svgs/Apple svg (1).svg";
-
-  static const String sp = "assets/svgs/Rectangle13(1).svg";
-  static const String google = "assets/svgs/Google.svg";
-  static const String facebook = "assets/svgs/Facebook.svg";
-}
--- e-commerce/lib/config/asset/svg_path.dart
+++ e-commerce/lib/config/asset/svg_path.dart
@@ -2,8 +2,9 @@ class SvgPath {
   SvgPath._();
 
   static const String ap = "assets/svgs/Apple svg (1).svg";
+  static const String splash = "assets/svgs/Rectangle.svg";
+  static const String go = "assets/svgs/Google.svg";
 
-  static const String sp = "assets/svgs/Rectangle13(1).svg";
   static const String google = "assets/svgs/Google.svg";
   static const String facebook = "assets/svgs/Facebook.svg";
 }
