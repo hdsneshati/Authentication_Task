f1a69a52c232cf0da651f7f138103ab444defd41
hdsneshati <hadisneshati1384@gmail.com>
2025-08-10 13:14:48 +0330

    add apicontext in utils

---
+++ e-commerce/lib/core/utils/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static const String domainName = "https://dummyjson.com/auth/";
+  static const String login = "login"; //send phone number as param
+  static const String me = "me";
+  static const String refreshToken = "refresh";
+}
